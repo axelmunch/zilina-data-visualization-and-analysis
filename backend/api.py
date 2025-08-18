6079737feee8b7c4804df350ad6ae5bc3e1394c0
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2025-08-18 13:56:41 +0200

    Extracted data

--- backend/api.py
+++ backend/api.py
@@ -5,7 +5,23 @@ app = Flask(__name__)
 
 
 def insert_data(data: Data):
-    print(data)
+    device_name = data["device"]
+    sensors = data["sensors"]
+    for sensor in sensors:
+        sensor_id = sensor["sensor_id"]
+        sensor_type = sensor["sensor_type"]
+        measurements = sensor["data"]
+
+        for measurement in measurements:
+            timestamp = measurement.get("timestamp")
+            for m in measurement["measurements"]:
+                name = m["name"]
+                value = m["value"]
+                print(
+                    f"{device_name} - {sensor_id} ({sensor_type}): {name} => {value} ({timestamp})"
+                )
+
+                # TODO: Add data point
 
 
 @app.post("/")
